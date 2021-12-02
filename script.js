8c243943febafbdd64387977b694a9d4de04a28d
Hamza Aamir <hamzaamirm2001@gmail.com>
2021-12-02 12:28:24 +0500

    Project Completed
    Not Responsive

--- script.js
+++ script.js
@@ -2,3 +2,19 @@
 window.addEventListener('load', function(){
     document.querySelector('.myPreloader').classList.add('noloader');
 }); 
+
+// Play-Pause Button
+function pause(){
+    const video = document.querySelector('.video-bg');
+    const btn = document.querySelector('.play-pause');
+    if(!btn.classList.contains('slide')){
+        btn.classList.add('slide');
+        document.getElementById('play-pause-img').src = 'play.png';
+        video.pause();
+    }
+    else{
+        btn.classList.remove('slide');
+        document.getElementById('play-pause-img').src = 'pause.png';
+        video.play();
+    }
+}
